f0d16afc7437e4fdb3f092ac5e8074fcaba4fb95
podhmo <ababjam61+github@gmail.com>
2018-05-30 08:15:58 +0900

    chore: examples

--- httptrace/_example/github/main.go
+++ httptrace/_example/github/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/google/go-github/github"
 	"github.com/k0kubun/pp"
+	"github.com/podhmo/go-traceable/httptrace"
 )
 
 func main() {
+	teardown := httptrace.Patch(os.Stderr)
+	defer teardown()
+
 	if err := run(); err != nil {
 		log.Fatalf("%+v", err)
 	}
